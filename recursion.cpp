215984415dc847c191fa0be76dbbe8821dc8f18e
MayankPandey2611 <kitupnd@gmail.com>
2025-06-03 17:15:36 +0530

    recursion quesstions

---
+++ recursion.cpp
@@ -0,0 +1,35 @@
+
+#include<iostream>
+using namespace std;
+
+// void table(int n,int i=1){
+
+//     if(i>10){
+//         return ;
+//     }
+
+//    cout<<n*i<<" ";
+
+//    table(n,i+1);
+// }
+
+
+// int main(){
+
+//     int x=5;
+//     table(5);
+
+// }
+
+
+// int pow(int n , int i){
+//     if(i==0){
+//         return 1;
+//     }
+//     return n * pow(n,i-1);
+// }
+
+// int main(){
+//     int num =9;
+//    cout<< pow(num,2);
+// }
