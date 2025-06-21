2f4a45d41d368bba6962bf7b14a59d78c0345013
MayankPandey2611 <kitupnd@gmail.com>
2025-06-21 17:34:14 +0530

    class template

--- templates.cpp
+++ templates.cpp
@@ -3,12 +3,29 @@
 using namespace std;
 
 // BY USING THE TEMPLATES WE DONT NEEDS TO INTIALIZE THE VALUES BY PARTICULAR PARAMETERS.
-template <class a,class b>
-void  sum(a x ,b y){
-    cout<< x+y;
+// template <class a,class b>
+// void  sum(a x ,b y){
+//     cout<< x+y;
+// }
+
+// int main(){
+
+//     sum(2.9,10.7);
+// };
+
+//  CLASS TEMPLATES 
+
+template <class obj1 >
+class job{
+public: obj1 v;
+
+job(obj1 val){
+    v=val;
+    cout<<v;
 }
+};
+
 
 int main(){
-
-    sum(2.9,10.7);
-};
+    job <int> j (21);
+}
