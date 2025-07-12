75ebaa3c1a6ae21575dbb69caad044d24acbdc9d
MayankPandey2611 <kitupnd@gmail.com>
2025-07-12 19:18:17 +0530

    dsa series and queue and deque

---
+++ DSA_QUESTIONS/DAY1.cpp
@@ -0,0 +1,107 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<list>
+using namespace std;
+
+int main(){
+
+    // QUESTION 1. FIND MAX ELEMENT...
+    // vector<int>v{21,3,47,8,9,10,20};
+
+    // int max = v[0];
+
+    // for(int i=0; i<v.size(); i++){
+    //     if(v[i] > max){
+    //         max=v[i];
+    //     }
+    // }
+
+    // cout<<max;
+
+
+
+    // QUESTION 2. COUNT EVEN AND ODD NUMBERS IN LIST....
+    // list<int>l{12,34,8,9,7,6,1,0,8,5};
+
+    // int evencount =0;
+    // int oddcount =0;
+    // int zero = 0;
+
+    // for(int i : l){
+    //     if(i == 0){
+    //         zero++;
+    //     }
+    //     else if( i % 2 == 0){
+    //         evencount++;
+    //     }
+    //     else{
+    //         oddcount++;
+    //     }
+    // }
+    // cout<<"Even :"<<evencount<<endl;
+    // cout<<"odd :"<<oddcount<<endl;
+    // cout<<"zero :"<<zero<<endl;
+    
+
+    // BY USING ITERATOR....
+
+    // list<int>::iterator it;
+    // for(it = l.begin() ; it != l.end(); ++it){
+    //     if(*it == 0){
+    //         zero++;
+    //     }
+    //     else if(*it % 2==0){
+    //         evencount++;
+    //     }
+    //     else{
+    //         oddcount++;
+    //     }
+    // }
+
+    //  cout<<"Even :"<<evencount<<endl;
+    // cout<<"odd :"<<oddcount<<endl;
+    // cout<<"zero :"<<zero<<endl;
+
+
+
+
+    // QUESTION 3. REVERSE A GIVEN STRING......
+
+    // string str = "mayank";
+
+    // int i= 0;
+    // int j = str.size()-1;
+
+    // while(i<j){
+    //     swap(str[i],str[j]);
+    //     i++;
+    //     j--;
+    // }
+    
+    // cout<<str;
+
+
+
+    // QUESTION 4. CHECK NUMBER IS PRIME OR NOT......
+
+    int num;
+    cin>>num;
+
+    bool check = false;
+   for(int i= 2; i*i<=num; i++){
+
+    if(num % i == 0){
+        check=true;
+    }
+
+   }
+
+   if(check){
+    cout<<" not prime";
+   }
+   else{
+    cout<<"prime";
+   }
+   
+}
--- STL/Queue.cpp
+++ STL/Queue.cpp
@@ -1,21 +1,66 @@
-#include<iostream>
-#include<queue>
-#include<vector>
-#include<algorithm>
+#include <iostream>
+#include <queue>
+#include <deque>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
+//---------------------------------------SINGLY QUEUE------------------------------------------------------#
+// int main(){
+//     queue<int>q;
+//     q.push(12);
+//     q.push(12);
+//     q.push(12);
 
-int main(){
-    queue<int>q;
-    q.push(12);
-    q.push(12);
-    q.push(12);
-    
-   cout<<q.size()<<endl;
-   cout<<q.back()<<endl;
+//    cout<<q.size()<<endl;
+//    cout<<q.back()<<endl;
 
-   while (!q.empty()){
-    cout<<q.front()<<" ";
-    q.pop();
-   }
+//    while (!q.empty()){
+//     cout<<q.front()<<" ";
+//     q.pop();
+//    }
+// }
+
+//------------------------------------------------DOUBLY QUEUE (DEQUE)--------------------------------------------------------//
+
+int main()
+{
+    deque<int> q1;
+
+    q1.push_front(12);
+    q1.push_front(12);
+    q1.push_front(12);
+    q1.push_back(10);
+    q1.push_back(10);
+
+    for (auto &p : q1)
+    {
+        cout << ++p << " ";
+    }
+    cout << endl;
+    for (auto t : q1)
+    {
+        cout << t << " ";
+    }
+    cout << endl;
+
+    // q1.pop_back();
+    // q1.pop_front();
+
+    for (auto t : q1)
+    {
+        cout << t << " ";
+    }
+    cout << endl;
+
+    // ADDING VALUES IN DEQUE WITHOUT USING INDEXING..........
+
+    deque<int>::iterator it = q1.begin();
+    q1.insert(it+2, 23);
+      for (auto t : q1)
+    {
+        cout << t << " ";
+    }
+    cout<<endl;
+
 }
