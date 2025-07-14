4d3d565014fffc6292c495442aee85634b15d108
MayankPandey2611 <kitupnd@gmail.com>
2025-07-14 21:15:59 +0530

    day 4

---
+++ DSA_QUESTIONS/Day4.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<list>
+#include<unordered_map>
+using namespace std;
+
+// QUESTION 11. COUNT THE FREQUENCY OF THE ELEMENTS OF THE VECTOR.......
+
+// int main(){
+//     vector<int>v{12,12,34,23,34,56,23,77,77,67,77};
+
+//     unordered_map<int,int> count;
+
+//     for(auto num:v){
+//         count[num]++;
+//     }
+
+//     for(auto k:count){
+//         cout<< k.first << "="<< k.second <<endl;
+//     }
+// }
+
+
+
+// QUESTION 12. FIND THE MISSING NUMBERS IN A RANGE FROM 1 TO N.........
+
+// int main(){
+//     vector<int>v{1,3,7,9,11};
+
+//     for(int i=0; i<v.size()-1; i++){
+//         if(v[i+1] - v[i] > 1){
+//             for(int j=v[i]+1; j<v[i+1]; j++){
+//                 cout<<j<<" ";
+                
+//             }
+//         }
+//     }
+//     cout<<endl;
+
+// }
+
+
+
+// QUESTION 13. CHECK IF TWO STRINGS ARE ANAGRAM OR NOT........
+
+string str ;
